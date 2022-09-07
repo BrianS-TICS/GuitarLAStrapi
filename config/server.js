7eff7155068ab35e7ab220ab44060cbeca6ea1aa
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-09-06 19:09:07 -0600

    Add proyecto guitarla strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff298d5b598b4e8ef6fbf69369075d21'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1914b06f-dc7e-44f8-a58d-0980acb54b66'
+};
